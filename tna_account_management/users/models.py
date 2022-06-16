ce4d298ed28f00418a11176a53de92bb004a2ca6
Andy Babic <andy.babic@torchbox.com>
2022-06-16 22:16:05 +0100

    User fields: Replace first_name and last_name fields with single name field, and add profile_override

--- tna_account_management/users/models.py
+++ tna_account_management/users/models.py
@@ -1,10 +1,35 @@
+from functools import cached_property
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    name = models.TextField(max_length=400)
     auth0_id = models.CharField(max_length=36, blank=True, null=True, unique=True)
     email_verified = models.BooleanField(default=False)
+    profile_override = models.JSONField(
+        blank=True,
+        default=None,
+        help_text="When set, self.profile will return this value instead of fetching real profile data from Auth0.",
+    )
+
+    def get_full_name(self):
+        return self.name
+
+    @cached_property
+    def name_segments(self):
+        return self.name.strip().split(" ")
+
+    @property
+    def first_name(self) -> str:
+        return self.name_segments[0]
+
+    @property
+    def last_name(self) -> str:
+        try:
+            return " ".join(self.name_segments[1:])
+        except IndexError:
+            return ""
 
     @classmethod
     def get_unique_username(cls, base: str, exclude_pk: int = None):
