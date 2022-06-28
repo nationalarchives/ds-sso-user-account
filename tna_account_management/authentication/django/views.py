c06281ece75e8a7a062bac079f00615e035e91b0
Andy Babic <andy.babic@torchbox.com>
2022-06-28 17:20:44 +0100

    Provide basic login functionality when auth0 is not configured

--- tna_account_management/authentication/django/views.py
+++ tna_account_management/authentication/django/views.py
@@ -1,15 +1,40 @@
-from urllib.parse import quote_plus
+from urllib.parse import urlparse
 
 from django.conf import settings
-from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import logout as auth_logout, login as auth_login
 from django.shortcuts import redirect, render
+
+from tbxforms.forms import BaseForm as HelperMixin
+from tbxforms.layout import Button
+
+
+class CrispyAuthenticationForm(HelperMixin, AuthenticationForm):
+    @property
+    def helper(self):
+        fh = super().helper
+        fh.layout.extend([
+            Button.primary(
+                name="submit",
+                type="submit",
+                value="Sign in",
+            ),
+        ])
+        return fh
 
 
 def login(request):
-    target_url = '/admin/login/'
-    if next := request.GET.get("next"):
-        target_url += f"?next={quote_plus(next)}"
-    return redirect(target_url)
+    form = CrispyAuthenticationForm(data=request.POST or None)
+    if request.POST and form.is_valid():
+        auth_login(request, form.get_user())
+        redirect_to = form.cleaned_data.get("next", settings.LOGIN_REDIRECT_URL)
+        if redirect_to != settings.LOGIN_REDIRECT_URL:
+            parsed = urlparse(redirect_to)
+            if parsed.netloc and parsed.netloc != request.META.get("HTTP_HOST"):
+                redirect_to = settings.LOGIN_REDIRECT_URL
+        return redirect(redirect_to)
+    print(form.errors)
+    return render(request, "patterns/pages/auth/login.html", {"form": form})
 
 
 def logout(request):
